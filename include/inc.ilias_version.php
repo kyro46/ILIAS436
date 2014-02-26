c60c99116823ef5f4ad41915819ab7941809abd7
Christoph Jobst <cjobst@wifa.uni-leipzig.de>
2014-02-26 16:55:42 +0100

    Initial Commit

---
+++ include/inc.ilias_version.php
@@ -0,0 +1,14 @@
+<?php
+/* Copyright (c) 1998-2012 ILIAS open source e-Learning e.V., Extended GPL, see docs/LICENSE */
+
+/**
+* sets ilias version (this file shouldn't be merged between cvs branches)
+*
+* @author Alex Killing <alex.killing@gmx.de>
+* @version $Id: inc.ilias_version.php 47029 2014-01-05 16:53:40Z akill $
+*
+* @package ilias-core
+*/
+define("ILIAS_VERSION", "4.3.6 2014-01-05");
+define("ILIAS_VERSION_NUMERIC", "4.3.6");			// must be always x.y.z: x, y and z are numbers
+?>
